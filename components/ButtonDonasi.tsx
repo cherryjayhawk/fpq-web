07eedb7aac9c88159f7a1009481ecdd6f15c818d
muhbintangr <muhbintangr19@gmail.com>
2024-01-01 22:24:00 +0700

    add component ButtonDonasi

---
+++ components/ButtonDonasi.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from './ui/button' 
+
+function ButtonDonasi() {
+  return (
+    <Button className={'rounded-full px-4 py-2 border-2 border-transparent bg-lime-500 text-white font-bold hover:text-lime-500 hover:border-lime-500 hover:bg-transparent mr-2'}>Donasi yuk!</Button>
+  )
+}
+
+export default ButtonDonasi
