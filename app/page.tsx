6667fb5add899294e90e675de7437e348a67be3f
muhbintangr <muhbintangr19@gmail.com>
2024-01-07 01:24:45 +0700

    add component Testimonial

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image'
 import Navbar from '@/components/Navbar'
 import LandingPage from '@/components/LandingPage'
+import PenyaluranDonasi from '@/components/PenyaluranDonasi'
+import Testimonial from '@/components/Testimonial'
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <LandingPage />
+      <PenyaluranDonasi />
+      <Testimonial />
     </>
   )
 }
