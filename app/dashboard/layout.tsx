abbb56479d4621e0167f1f9da52e9f06fbe06be0
muhbintangr <muhbintangr19@gmail.com>
2024-01-08 02:30:01 +0700

    update admin layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,17 @@
+import { MainNav } from "@/components/NavbarAdmin"
+
 function DashboardLayout({ children }: { children: React.ReactNode }) {
     return (
-      <div>{ children }</div>
+      <div className="flex justify-between items-center w-full max-w-7xl px-4 py-6 mx-auto bg-transparent">
+          <div className="hidden w-full min-h-screen h-full flex-col md:flex">
+            <div className="border-b">
+              <div className="flex h-16 items-center px-4">
+                <MainNav className="mx-6" />
+              </div>
+            </div>
+            { children }
+          </div>
+      </div>
     )
   }
   
