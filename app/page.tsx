4f1b93c1c6cfd99779fb62e21859b0407895c4ca
muhbintangr <muhbintangr19@gmail.com>
2024-01-27 14:21:47 +0700

    add section Aktivitas

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
-import Image from 'next/image'
 import Navbar from '@/components/Navbar'
 import LandingPage from '@/components/LandingPage'
 import PenyaluranDonasi from '@/components/PenyaluranDonasi'
 import Testimonial from '@/components/Testimonial'
 import SocialMedia from '@/components/SocialMedia'
 import Footer from '@/components/Footer'
+import Aktivitas from '@/components/Aktivitas'
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <LandingPage />
+      <Aktivitas />
       <PenyaluranDonasi />
       <Testimonial />
       <Footer />  
---
+++ components/Aktivitas.tsx
@@ -0,0 +1,12 @@
+function Aktivitas() {
+  return (
+    <div className="w-full min-h-[80vh] flex flex-col items-center px-4 py-6 justify-evenly bg-gradient-to-br from-cyan-500 from-60% to-teal-400 md:-translate-y-0">
+        <h2 className='text-center text-4xl md:text-4xl font-bold text-sky-100'>Aktivitas</h2>
+        <div className="flex justify-between items-center max-w-7xl my-16 mx-auto w-full aspect-video border-white border-2 bg-sky-50">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Aktivitas
