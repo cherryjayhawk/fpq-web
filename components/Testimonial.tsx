7142746dedc008442ca51246c925fd71f28f3192
muhbintangr <muhbintangr19@gmail.com>
2024-01-27 14:23:21 +0700

    fix layout shift

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -2,7 +2,7 @@ import { CarouselAuto } from "./CarouselAuto"
 
 function Testimonial() {
   return (
-    <div className="w-full min-h-[80vh] flex flex-col items-center justify-evenly bg-gradient-to-br from-cyan-500 from-60% to-teal-400 -translate-y-96 md:-translate-y-16">
+    <div className="w-full min-h-[80vh] flex flex-col items-center py-6 justify-evenly bg-gradient-to-br from-cyan-500 from-60% to-teal-400 md:-translate-y-0">
         <h1 className='text-center text-4xl md:text-4xl font-bold text-sky-100'>Testimonial</h1>
         <div className="flex justify-between items-center w-full max-w-7xl px-4 py-6 mx-auto ">
             <CarouselAuto />
